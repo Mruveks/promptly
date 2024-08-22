6db5cece2f9a7136df18b8dea8453344c7a35957
Mruveks <Mruvekss@gmail.com>
2024-08-22 15:27:42 +0200

    Setup components

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,21 @@
+import Feed from "@components/Feed";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="mad-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p>
+        Promptly is an open-source AI prompting platform designed for the modern era, enabling users to explore, craft,
+        and share innovative prompts.
+      </p>
 
-export default page
+      <Feed />
+    </section>
+  );
+};
+
+export default page;
